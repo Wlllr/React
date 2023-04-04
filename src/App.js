9cc695ac2012894bba509bc0bcfcc724b19dd1fe
Weller <weller_pereira@hotmail.com>
2023-04-04 08:51:15 -0300

    monster-rolodex

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import { Component } from 'react';
+
+import logo from './logo.svg';
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
+import './App.css';
+
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState(() => {
+        return {monsters: users}
+      }
+      ));
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  }
+
+  render() {
+
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    return (
+      <div className="App">
+        <SearchBox
+          className='monsters-search-box'
+          placeholder='search monsters'
+          onChangeHandler = { onSearchChange }
+        />
+        <CardList monsters={filteredMonsters}/>
+      </div>
+    );
+  }
+}
+export default App;
